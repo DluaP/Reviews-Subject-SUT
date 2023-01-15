a29f13e98df9cdd480c7430e96058d20b64cd13b
DluaP <g.143.vasco@gmail.com>
2023-01-16 06:12:15 +0700

    12

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,16 +1,30 @@
-import { Card, Input, TabsProps, Form, Button, Tabs, Row, Col ,Image, notification} from "antd";
-import { fireNotification } from "./notification"
+import {
+  Card,
+  Input,
+  TabsProps,
+  Form,
+  Button,
+  Tabs,
+  Row,
+  Col,
+  Image,
+  notification,
+  Empty,
+} from "antd";
+import { fireNotification } from "./notification";
 const Login = () => {
-
   const signin = (element: any) => {
-    fireNotification({ type: "success"});
+    fireNotification({ type: "success" });
   };
   const signup = (element: any) => {
-    if(element.confirm != element.password){
+
+      if (element.confirm != element.password) {
         fireNotification({ type: "error", description: "รหัสไม่ตรงกัน" });
-    }else{
-        fireNotification({ type: "success"});
-    }
+      } else {
+        console.log(element);
+        
+        fireNotification({ type: "success" });
+      }
   };
   const onChange = (key: string) => {
     console.log(key);
@@ -23,7 +37,7 @@ const Login = () => {
       children: (
         <div>
           <Form layout="vertical" onFinish={signin}>
-          <Row gutter={[12, 0]}>
+            <Row gutter={[12, 0]}>
               <Col span={24}>
                 <Form.Item name="username" label="Username">
                   <Input placeholder="Username" />
@@ -31,7 +45,7 @@ const Login = () => {
               </Col>
               <Col span={24}>
                 <Form.Item name="password" label="Password">
-                  <Input placeholder="Password"  type="password"/>
+                  <Input placeholder="Password" type="password" />
                 </Form.Item>
               </Col>
             </Row>
@@ -55,27 +69,27 @@ const Login = () => {
               </Col>
               <Col span={12}>
                 <Form.Item name="firstname" label="Firstname">
-                  <Input placeholder="Firstname"  />
+                  <Input placeholder="Firstname" />
                 </Form.Item>
               </Col>
               <Col span={12}>
                 <Form.Item name="surname" label="Surname">
-                  <Input placeholder="Surname"  />
+                  <Input placeholder="Surname" />
                 </Form.Item>
               </Col>
               <Col span={24}>
                 <Form.Item name="nickname" label="Nickname">
-                  <Input placeholder="Nickname"  />
+                  <Input placeholder="Nickname" />
                 </Form.Item>
               </Col>
               <Col span={24}>
                 <Form.Item name="password" label="Password">
-                  <Input placeholder="Password"  type="password"/>
+                  <Input placeholder="Password" type="password" />
                 </Form.Item>
               </Col>
               <Col span={24}>
                 <Form.Item name="confirm" label="Confirm Password">
-                  <Input placeholder="Confirm Password"  type="password"/>
+                  <Input placeholder="Confirm Password" type="password" />
                 </Form.Item>
               </Col>
             </Row>
@@ -89,11 +103,16 @@ const Login = () => {
   ];
   return (
     <div className="bg-[#F9ECCE] w-[100%] h-[100vh] ">
-        <div className="text-center pt-20">
-        <Image  src="./images/Logo.png" preview={false}/>
+      <div className="text-center pt-20">
+        <Image src="./images/Logo.png" preview={false} />
       </div>
       <div className="px-[50vh]   !content-center">
-        <Tabs defaultActiveKey="1" items={items} onChange={onChange} className="bg-white px-10  pt-4 pb-10 rounded-lg"/>
+        <Tabs
+          defaultActiveKey="1"
+          items={items}
+          onChange={onChange}
+          className="bg-white px-10  pt-4 pb-10 rounded-lg"
+        />
       </div>
     </div>
   );
