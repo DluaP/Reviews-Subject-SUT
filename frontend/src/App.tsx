154015adf61f6ade66fa8127858c440fc04acd34
DluaP <g.143.vasco@gmail.com>
2023-01-27 01:59:43 +0700

    11

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import Home from "./components/Home";
-import Login from "./components/login";
-import CreatePost from "./components/create-post";
-import Review from "./components/review";
+
 import { useNavigate,BrowserRouter,Route, Routes, useRoutes } from "react-router-dom";
 import { APP_ROUTER } from "./router";
 
